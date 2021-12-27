e43b40f830e703756678d955a3837a47308b3ba1
RomanDorosh <roma.dorosh@gmail.com>
2021-12-27 19:07:40 +0100

    Added links to footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
     return (
         <div className='footer'>
-            {/* Have a look at my linkedin profile */}
+            <h3>
+                <a className='footer-link' href='https://www.linkedin.com/in/roman-dorosh-2b26121bb/' target='_blank'><FaLinkedin /></a>
+                <a className='footer-link' href='https://github.com/RomanDorosh' target='_blank'><FaGithub /></a>
+            </h3>
         </div>
     )
 }
