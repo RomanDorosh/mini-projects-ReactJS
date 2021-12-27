422844dc692a86a0aa8f21e700c186bda7ad5fa1
RomanDorosh <roma.dorosh@gmail.com>
2021-12-27 18:41:24 +0100

    Finished with Roman Numeral Converter project

--- src/pages/RomanNumeralConverter.jsx
+++ src/pages/RomanNumeralConverter.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const RomanNumeralConverter = () => {
+    const [result, setResult] = useState("")
+    const [number, setNumber] = useState("")
+    const [error, setError] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        try {
+            let lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+            let roman = '', i;
+            let num = number;
+
+            for (i in lookup) {
+                while (num >= lookup[i]) {
+                    roman += i;
+                    num -= lookup[i];
+                }
+            }
+            setResult(number === "" ? "Enter some number" : roman);
+        } catch (error) {
+            setError(true);
+            console.log(error);
+        }
+    }
+
     return (
         <main>
             <section className="project section-center">
@@ -9,7 +34,19 @@ const RomanNumeralConverter = () => {
                     <h3>roman numeral converter</h3>
                     <div className="underline"></div>
                 </div>
-                <h4>This project is currently under development...</h4>
+                <h4>{result}</h4>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type='number'
+                        value={number}
+                        onChange={(e) => setNumber(e.target.value)}
+                        placeholder='Enter some number'
+                        className={`${error ? 'error' : null}`}
+                    />
+                    <button className='btn' type='submit'>
+                        submit
+                    </button>
+                </form>
                 <Link to="/"><button className='btn btn-back'>go back</button></Link>
             </section>
         </main>
